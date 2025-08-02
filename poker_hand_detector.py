900e2cd17b905bef66c45a341c4fcadc8e1381f2
Binaya shrestha <sthavicky555@gmail.com>
2025-08-02 21:11:42 +0545

    added function to simulate the iteration and save video

--- poker_hand_detector.py
+++ poker_hand_detector.py
@@ -3,25 +3,19 @@ import cv2 as cv
 import math
 import poker_hand_func
 import label_func
+from cards_names import classNames
+import simulation
+import save_video_func
 
 cap = cv.VideoCapture(0)  # For Webcam
+#cap = cv.VideoCapture("Poker_hand_detection_using_yolo\\resources\\video2.mp4")  # Video file
+#output_video_url = "Poker_hand_detection_using_yolo\output_video\\1.mp4"
 cap.set(3, 1280)
 cap.set(4, 720)
 
 model = YOLO('Poker_hand_detection_using_yolo\playingCards.pt')
-classNames = ['10C', '10D', '10H', '10S',
-              '2C', '2D', '2H', '2S',
-              '3C', '3D', '3H', '3S',
-              '4C', '4D', '4H', '4S',
-              '5C', '5D', '5H', '5S',
-              '6C', '6D', '6H', '6S',
-              '7C', '7D', '7H', '7S',
-              '8C', '8D', '8H', '8S',
-              '9C', '9D', '9H', '9S',
-              'AC', 'AD', 'AH', 'AS',
-              'JC', 'JD', 'JH', 'JS',
-              'KC', 'KD', 'KH', 'KS',
-              'QC', 'QD', 'QH', 'QS']
+
+#video_write = save_video_func.video_writer(output_video_url, fps=30, frame_size=(1280, 720))
 
 while True:
     ret, frame = cap.read()
@@ -35,8 +29,7 @@ while True:
         boxes = r.boxes
         for box in boxes:
             #bounding box
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
             
             #confidence
             conf = math.ceil((box.conf[0] * 100)) / 100
@@ -64,24 +57,39 @@ while True:
             if conf > 0.3:
                 hand.append(card_label)
     
+    #remove copies of cards
     hand = list(set(hand))
+    player_cards = list(set(player_cards))
+    community_cards = list(set(community_cards))
 
+    # Only run simulation when player has 2 hole cards and at least 3 community cards
     if len(player_cards) == 2 and len(community_cards) >= 3:
-        full_hand = player_cards + community_cards
+        #simulate to get the probabilities
+        estimated_prob = simulation.simulate(player_cards, community_cards)
+
+        full_hand = player_cards + community_cards  # Combine both to form a 7-card hand
+
         if len(full_hand) >= 5:
-            rank, best_hand = poker_hand_func.findPokerHand(full_hand[:7])
-            print(rank)
-            print(best_hand)
+            # Evaluate best possible hand from the available cards
+            rank, best_hand, rank_score = poker_hand_func.findPokerHand(full_hand[:7])
+            print(rank)        
+            print(best_hand)   
+
 
         #display hand
         label_func.show_hand(frame, f'Your hand: {rank}, Your card: {best_hand}')
+        label_func.show_hand(frame, f"Estimated Probability of winning: {estimated_prob:.4f}", start_pos=(10, line_y))
 
     #display line that seprates player and community card
     cv.line(frame, (0, line_y), (frame.shape[1], line_y), (0, 0, 255), 2)
+
+    #save_video_func.save_video(video_write, frame)
+
     cv.imshow("webcam", frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
+#video_write.release
 cap.release()
 cv.destroyAllWindows()
 
